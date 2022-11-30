f09fc774cbed85e35ab8cfa51d009cddfa622d74
unknown <alexanderfalkenberg9@gmail.com>
2022-11-30 12:24:54 +0100

    Änderung damit header und footer dynamisch hinzugefügt werden

--- website/client/src/scripts/Main.mjs
+++ website/client/src/scripts/Main.mjs
@@ -1,5 +1,9 @@
 import { moveMenu, burgerMenu, bookingInput } from './Header.mjs';
+import $ from 'jquery';
 
-moveMenu();
-burgerMenu();
-bookingInput();
+$('header').load('header.html header', function () {
+  moveMenu();
+  burgerMenu();
+  bookingInput();
+});
+$('footer').load('footer.html footer');
