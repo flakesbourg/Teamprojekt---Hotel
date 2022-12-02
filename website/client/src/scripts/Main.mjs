3e7d9f485215c61552263d7ee737fd78e7f4ef28
unknown <alexanderfalkenberg9@gmail.com>
2022-12-02 09:02:02 +0100

    header und footer fertiggestellt

--- website/client/src/scripts/Main.mjs
+++ website/client/src/scripts/Main.mjs
@@ -1,12 +1,12 @@
 import { moveMenu, burgerMenu, bookingInput } from './Header.mjs';
 import $ from 'jquery';
 
-$('header').load('header.html header', function () {
-  window.scrollTo({
-    top: 0
-  });
+$('header').load('header.html #header', function () {
   moveMenu();
   burgerMenu();
   bookingInput();
+  window.scrollTo({
+    top: 0
+  });
 });
-$('footer').load('footer.html footer');
+$('footer').load('footer.html #footer');
