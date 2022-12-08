113c830653ff36d8465ba18c55d7ba207746ef53
unknown <alexanderfalkenberg9@gmail.com>
2022-12-08 13:21:50 +0100

    HeroImages für die Zimmer hinzugefügt

--- website/client/src/scripts/Main.mjs
+++ website/client/src/scripts/Main.mjs
@@ -9,6 +9,26 @@ $('header').load('header.html #header', function () {
   window.scrollTo({
     top: 0
   });
+
+  const title = document.title;
+  switch (title) {
+    case 'Zimmer':
+      $('#logo').replaceWith('<h1 id="headline">Zimmer</h1>');
+      document.getElementsByClassName('heroImageHomepage')[0].classList.add('heroImageZimmer');
+      break;
+    case 'Zimmer - Basic':
+      $('#logo').replaceWith('<h1 id="headline">Basic</h1>');
+      document.getElementsByClassName('heroImageHomepage')[0].classList.add('heroImageBasic');
+      break;
+    case 'Zimmer - Family':
+      $('#logo').replaceWith('<h1 id="headline">Family</h1>');
+      document.getElementsByClassName('heroImageHomepage')[0].classList.add('heroImageFamily');
+      break;
+    case 'Zimmer - Premium':
+      $('#logo').replaceWith('<h1 id="headline">Premium</h1>');
+      document.getElementsByClassName('heroImageHomepage')[0].classList.add('heroImagePremium');
+      break;
+  }
 });
 $('footer').load('footer.html #footer');
 
